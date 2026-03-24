5fcf37a0a80b1c4212b49976586f725fe35ef82e
Roman <romario.higa@gmail.com>
2026-03-24 19:28:23 +0100

    second

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,20 +1,20 @@
 function ArticleTemplate(article) {
-    let { autor, title, description, publishedAt, url, urlToImage,
+  const deff =
+    'https://static6.depositphotos.com/1026266/543/i/450/depositphotos_5437053-stock-photo-woman-pressing-modern-error-button.jpg';
+    let { autor, title, description, publishedAt, url, urlToImage = deff,
     } = article;
 
     return `
-      <li>
-    <h2>${title}</h2>
-    <img src="${urlToImage}" alt="">
-    <p>${autor}</p>
-    <p>${description}</p>
-    <a href="${url}">Джерело</a>
-    <span>${publishedAt}</span>
-  </li>
-
-    `;
+      <li class="li-elem">
+    <h2 class="title-h2">${title}</h2>
+    <img class="image" src="${urlToImage}" alt="">
+    ${autor ? `<p class="autor">${autor}</p>` : ''}
+    <p class="decr">${description}</p>
+    <a class="link" href="${url}">Посилання на джерело</a>
+    <p class="date"> Was posted ${new Date(publishedAt).toLocaleString()}</p>
+  </li>`;
 };
 
 export function ArticlesTemplate(articles) {
-    return articles.map(ArticleTemplate).join();
+    return articles.map(ArticleTemplate).join('');
 }
